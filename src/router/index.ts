4ec5500bfe656c297b292422037c17df6fcd262a
mnazarani <mnazarani@gmail.com>
2024-08-28 15:45:36 -0500

    added dynamic import for projects route in router file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import ProjectView from '@/views/ProjectView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +14,7 @@ const router = createRouter({
     {
       path: '/projects',
       name: 'projects',
-      component: ProjectView
+      component: () => import('@/views/ProjectView.vue')
     }
   ]
 })
